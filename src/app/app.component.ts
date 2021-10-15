dd39864b2b7d937b97711b65ff8b317e65827858
Enzo <enzo.biamonti@epitech.eu>
2021-10-15 11:06:16 +0200

    public key, api url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ class NewsletterService {
 
 export class AppComponent {
   title = 'AngularPwa';
-  readonly VAPID_PUBLIC_KEY = "BLBx-hf2WrL2qEa0qKb-aCJbcxEvyn62GDTyyP9KTS5K7ZL0K7TfmOKSPqp8vQF0DaG8hpSBknz_x3qf5F4iEFo";
+  readonly VAPID_PUBLIC_KEY = "BNd5N-Y2tk_p5jcJmwa_bXP_ryoc9HXFeRkY0XV6RbvV3FCdM-5kHo9MzJSeMksQOilOLQyQy-CMsIFSr0O19kk";
 
   constructor(
     private swPush: SwPush,
